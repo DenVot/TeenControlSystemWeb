80d423ddb51dd3c41805b57f3daec64c297e6341
Denis <denis@denvot.xyz>
2022-02-27 14:13:30 +0300

    Binding extension

---
+++ TeenControlSystemWeb/Extensions/SensorExtensions.cs
@@ -0,0 +1,16 @@
+using TeenControlSystemWeb.Exceptions.Sensor;
+
+namespace TeenControlSystemWeb.Extensions;
+
+public static class SensorExtensions
+{
+    public static void BindSensorToSession(this Sensor sensor, Session session)
+    {
+        if (sensor.SessionId != null)
+        {
+            throw new SensorAlreadyInUseException(sensor.Id);
+        }
+            
+        sensor.SessionId = session.Id;
+    }
+}
