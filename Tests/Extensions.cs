ba33c92f4a2b79c9e8a2d2672bd2cf5df5007d5d
Denis <denis@denvot.xyz>
2022-03-02 22:18:43 +0300

    Remove redundant code

--- Tests/Extensions.cs
+++ Tests/Extensions.cs
@@ -1,19 +1,12 @@
 using Moq;
 using TeenControlSystemWeb.Data.Repositories;
+using TeenControlSystemWeb.Helpers;
 using TeenControlSystemWeb.Providers;
 
 namespace Tests;
 
 public static class Extensions
 {
-    public static void InitializeMock(this Mock<IDataProvider> mock)
-    {
-        mock.Setup(x => x.PointsRepository);
-        mock.Setup(x => x.SensorsRepository);
-        mock.Setup(x => x.SessionsRepository);
-        mock.Setup(x => x.UsersRepository);
-        mock.Setup(x => x.UserAuthorizationTokensRepository);
-    }
-    
     public static SessionProvider ConfigureSessionProvider(this Mock<IDataProvider> mock) => new(mock.Object);
+    public static PasswordComparator ConfigurePasswordComparator(this Mock<IDataProvider> mock) => new(mock.Object);
 }
