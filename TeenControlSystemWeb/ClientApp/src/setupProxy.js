da73044c410b0e9576b73c375464b85dbbf95cfc
Denis <denis@denvot.xyz>
2022-03-17 10:18:36 +0300

    Setup proxy to get access to API and Swagger

--- TeenControlSystemWeb/ClientApp/src/setupProxy.js
+++ TeenControlSystemWeb/ClientApp/src/setupProxy.js
@@ -5,7 +5,9 @@ const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_H
   env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:4383';
 
 const context =  [
-  "/weatherforecast",
+  "/api/",
+  "/index.html",
+  "/swagger"
 ];
 
 module.exports = function(app) {
