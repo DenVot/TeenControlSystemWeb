71d056e064f13556bb6c87411657630e856e5273
Denis <denis@denvot.xyz>
2022-03-17 10:16:46 +0300

    Exceptions

--- TeenControlSystemWeb/Exceptions/User/UserAlreadyExistsWithContextUsernameException.cs
+++ TeenControlSystemWeb/Exceptions/User/UserAlreadyExistsWithContextUsernameException.cs
@@ -2,5 +2,8 @@ namespace TeenControlSystemWeb.Exceptions.User;
 
 public class UserAlreadyExistsWithContextUsernameException : SystemException
 {
-    
+    public UserAlreadyExistsWithContextUsernameException() : base("Пользователь с таким именем уже существует")
+    {
+        
+    }
 }
