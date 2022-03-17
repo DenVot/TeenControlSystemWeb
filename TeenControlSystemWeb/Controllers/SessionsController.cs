c644bfec562cb5a254b46c832717838c64182138
Denis <denis@denvot.xyz>
2022-03-17 11:01:56 +0300

    Session API

--- TeenControlSystemWeb/Controllers/SessionsController.cs
+++ TeenControlSystemWeb/Controllers/SessionsController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using TeenControlSystemWeb.Attributes;
 using TeenControlSystemWeb.Extensions;
+using TeenControlSystemWeb.Helpers;
 using TeenControlSystemWeb.Providers;
 
 namespace TeenControlSystemWeb.Controllers;
@@ -31,6 +32,62 @@ public class SessionsController : ControllerBase
                 data.FromPoint,
                 data.ToPoint);
 
+            return Ok();
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e.Message);
+        }
+    }
+    
+    [HttpPost("start-session")]
+    public async Task<ActionResult> StartSession([FromQuery] long sessionId)
+    {
+        try
+        {
+            await _sessionProvider.StartSessionAsync(sessionId);
+            return Ok();
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e.Message);
+        }
+    }
+
+    [HttpPost("end-session")]
+    public async Task<ActionResult> EndSession([FromQuery] long sessionId)
+    {
+        try
+        {
+            await _sessionProvider.EndSessionAsync(sessionId);
+            return Ok();
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e.Message);
+        }
+    }
+
+    [HttpPatch("edit-session")]
+    public async Task<ActionResult> EditSession([FromBody]SessionDelta delta, [FromQuery]long sessionId)
+    {
+        try
+        {
+            await _sessionProvider.EditSessionAsync(sessionId, delta);
+            return Ok();
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e.Message);
+        }
+    }
+
+    [HttpPatch("update-state")]
+    public async Task<ActionResult> UpdateSessionState([FromQuery] long sessionId, [FromBody] SessionSnapshot snapshot)
+    {
+        try
+        {
+            await _sessionProvider.UpdateSessionStateAsync(sessionId, snapshot);
             return Ok();
         }
         catch (Exception e)
